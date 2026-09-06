5b3da027eb75bf7cb09b0236c558acc0273787b7
MeraajFar <ma.faruqui.2023@smu.edu.sg>
2026-09-06 21:18:06 +0800

    q2 test commit

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,47 +1,47 @@
 public class SinglyLinkedList<E> {
-    
+
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
 
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first(){
-        if (isEmpty()){
+    public E first() {
+        if (isEmpty()) {
             return null;
-        } 
+        }
         return head.getElement();
     }
 
-    public E last(){
-        if (isEmpty()){
+    public E last() {
+        if (isEmpty()) {
             return null;
         }
         return tail.getElement();
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
         head = new Node<>(e, head);
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = head;
         }
         size++;
     }
 
-    public void addLast(E e){
+    public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newest;
         } else {
             tail.setNext(newest);
@@ -50,8 +50,8 @@ public class SinglyLinkedList<E> {
         size++;
     }
 
-    public E removeFirst(){
-        if (isEmpty()){
+    public E removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
@@ -59,7 +59,7 @@ public class SinglyLinkedList<E> {
         head = head.getNext();
         size--;
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = null;
         }
         return answer;
@@ -102,7 +102,6 @@ public class SinglyLinkedList<E> {
         current.setNext(null);
         tail = current;
         size--;
-
         return answer;
     }
 
