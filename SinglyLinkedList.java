4a8e8833f2bd00bb58386f4fc5cbfdf55f5f4470
MeraajFar <ma.faruqui.2023@smu.edu.sg>
2026-09-06 21:16:28 +0800

    q2 first commit

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -66,15 +66,61 @@ public class SinglyLinkedList<E> {
     }
 
     // Write your codes below
-    public String toString(){
-     
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        Node<E> current = head;
+
+        while (current != null) {
+            result.append(current.getElement());
+            current = current.getNext();
+        }
+
+        return result.toString();
     }
 
-    public E removeLast(){
-      
+    public E removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        E answer = tail.getElement();
+
+        if (size == 1) {
+            head = null;
+            tail = null;
+            size = 0;
+            return answer;
+        }
+
+        Node<E> current = head;
+
+        while (current.getNext() != tail) {
+            current = current.getNext();
+        }
+
+        current.setNext(null);
+        tail = current;
+        size--;
+
+        return answer;
     }
 
-    public void reverse(){       
-                 
+    public void reverse() {
+        Node<E> previous = null;
+        Node<E> current = head;
+
+        tail = head;
+
+        while (current != null) {
+            Node<E> next = current.getNext();
+
+            current.setNext(previous);
+
+            previous = current;
+            current = next;
+        }
+
+        head = previous;
     }
 }
